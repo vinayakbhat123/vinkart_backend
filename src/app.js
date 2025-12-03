8246e4e7a733397334317b738df3546aad786254
vinayakbhat123 <abcdbhat014@gmail.com>
2025-12-03 22:25:19 +0530

    change the cors place

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ require("dotenv").config()
 const app = express();
 const PORT = process.env.PORT || 3000
 // middleware
-app.use(express.json())
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
-}))
+}));
+app.use(express.json())
 app.use(cookieParser());   // must be before routes
 
 app.use("/",authRouter)
